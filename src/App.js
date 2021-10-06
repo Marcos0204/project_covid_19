9ae321a27dc5624a933954a54ccda01e626a41ec
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-10-06 01:11:57 -0500

    export-defaul-as-components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+
+import { Cards, Chart, CountryPicker } from './components';
 
 const App = () => {
     return (
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Cards = () => {
     return (
         <div>
-            
+            <h1>Cards</h1>
         </div>
     )
 }
