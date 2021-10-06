93febb5363ad521d7580c714d309ab1585d6d216
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-10-06 02:41:20 -0500

    making the request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './App.module.css'
 
 import { Cards, Chart, CountryPicker } from './components';
+import { fetchData } from './api';
 
 const App = () => {
+
+    const [ data, setData ] = useState({}) 
+
+    useEffect(  ()=>{
+        
+            async function fecht() {
+                const  data  = await fetchData();
+                setData(data)
+            }
+            fecht()
+            
+        
+    }, [])
+    console.log(data)
     return (
-        <div>
-            <h1>aspp</h1>
+        <div className={styles.container}>
+            <Cards/>
+            <Chart/>
+            <CountryPicker/>
         </div>
     )
 }
