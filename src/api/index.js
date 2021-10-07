33384b1f02e2f2576e54ef09c7029710e084e35f
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-10-07 00:01:17 -0500

    update-full-app

--- src/api/index.js
+++ src/api/index.js
@@ -17,3 +17,23 @@ export const fetchData = async (country) => {
     return error;
   }
 };
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
+
+    return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchCountries = async () => {
+try {
+  const { data: { countries } } = await axios.get(`${url}/countries`);
+
+  return countries.map((country) => country.name);
+} catch (error) {
+  return error;
+}
+};
