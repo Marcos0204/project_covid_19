b034b3c7ca981580ce1087336c5d29138e260913
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-10-06 21:44:00 -0500

    cards-update-styles

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,51 +1,41 @@
 import React from 'react';
-import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
+import CardComponent from './Card/Card';
 import styles from './Cards.module.css';
-import CountUp from 'react-countup';
-import cx from 'classnames'
 
-const Cards = ({data: { confirmed, deaths, recovered, lastUpdate } }) => {
-    
-    ///console.log(recovered)
-    if(!confirmed){
-        return <p>loading...</p>
-    }
-    return (
-        <div className={styles.container}>
-            <Grid container spacing={3} justify='center' >
-                <Grid item component={Card} xm={12} md={3} className={cx(styles.card, styles.infected)}>
-                    <CardContent>
-                        <Typography gutterBottom color='textSecondary' >Infected</Typography>
-                        <Typography variant='h5'>
-                            <CountUp start={0} end={confirmed.value} duration={2.5} separator=','/>
-                        </Typography>
-                        <Typography color='textSecondary' >{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant='body2'> Number of cases of covid_19</Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item component={Card} xm={12} md={3} className={cx(styles.card, styles.recovered)} >
-                    <CardContent>
-                        <Typography gutterBottom color='textSecondary' >Recovered</Typography>
-                        <Typography variant='h5'>
-                            <CountUp start={0} end={recovered.value} duration={2.5} separator=','/>
-                        </Typography>
-                        <Typography color='textSecondary' >{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant='body2'> Number of recoveries from covid_19</Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item component={Card} xm={12} md={3} className={cx(styles.card, styles.deaths)} >
-                    <CardContent>
-                        <Typography gutterBottom color='textSecondary' >Deaths </Typography>
-                        <Typography variant='h5'>
-                            <CountUp start={0} end={deaths.value} duration={2.5} separator=','/>
-                        </Typography>
-                        <Typography color='textSecondary' >{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant='body2'> Number of deaths caused by covid_19</Typography>
-                    </CardContent>
-                </Grid>
-            </Grid>
-        </div>
-    )
-}
+const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return 'Loading...';
+  }
 
-export default Cards
+  return (
+    <div className={styles.container}>
+        <Typography gutterBottom variant="h4" className={styles.title}  component="h2">Global</Typography>
+        <Grid container spacing={3} justify="center" className={styles.gridContainer}>
+            <CardComponent
+            className={styles.infected}
+            cardTitle="Infected"
+            value={confirmed.value}
+            lastUpdate={lastUpdate}
+            cardSubtitle="Number of active cases from COVID-19."
+            />
+            <CardComponent
+            className={styles.recovered}
+            cardTitle="Recovered"
+            value={recovered.value}
+            lastUpdate={lastUpdate}
+            cardSubtitle="Number of recoveries from COVID-19."
+            />
+            <CardComponent
+            className={styles.deaths}
+            cardTitle="Deaths"
+            value={deaths.value}
+            lastUpdate={lastUpdate}
+            cardSubtitle="Number of deaths caused by COVID-19."
+            />
+        </Grid>
+    </div>
+    );
+};
+
+export default Info;
